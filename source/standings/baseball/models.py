649f09a3ee2b0058d946255ba2dbc1d6112f6387
lukehunter <luke.hunter@gmail.com>
2010-06-13 23:54:33 -0700

    prettified, more working. http://croppy.lukehunter.net/shots/8281b666-059a-4d3b-ab82-d49ba62b8e0d.png

--- source/standings/baseball/models.py
+++ source/standings/baseball/models.py
@@ -8,20 +8,25 @@ class League(models.Model):
 class Division(models.Model):
    name = models.CharField(max_length=64)
    league = models.ForeignKey(League)
-   def __unicode__(self):
+   def tostring(self):
       return self.league.name + " " + self.name
+   def __unicode__(self):
+      return self.tostring()
    
 class Team(models.Model):
    name = models.CharField(max_length=64)
    city = models.CharField(max_length=64)
    abbrev = models.CharField(max_length=3)
-   streak = models.CharField(max_length=10)
-   wins = models.IntegerField()
-   losses = models.IntegerField()
-   score = models.IntegerField()
-   opponent_score = models.IntegerField()
-   opponent_abbrev = models.CharField(max_length=3)
-   next_game = models.DateTimeField('next game')
+   streak = models.CharField(max_length=10, blank=True, null=True)
+   wins = models.IntegerField(blank=True, null=True)
+   losses = models.IntegerField(blank=True, null=True)
+   score = models.IntegerField(blank=True, null=True)
+   opponent_score = models.IntegerField(blank=True, null=True)
+   opponent_abbrev = models.CharField(max_length=3, blank=True, null=True)
+   next_game = models.DateTimeField('next game', blank=True, null=True)
    division = models.ForeignKey(Division)
+   
+   def win_pct(self):
+      return float(self.wins) / (float(self.wins) + float(self.losses))
    def __unicode__(self):
       return self.abbrev
--- source/standings/baseball/views.py
+++ source/standings/baseball/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from standings.baseball.models import Team, League, Division
+from itertools import *
 
 def index(request):
    team_list = Team.objects.all()
-   return render_to_response('baseball/index.html', {'team_list': team_list})
+   divisionmap = dict((k,list(v)) for k,v in groupby(team_list, key=lambda team: team.division.league.name + " " + team.division.name))
+   return render_to_response('baseball/index.html', {'divisionmap': divisionmap})
